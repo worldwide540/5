2662a97c3cead20e365d1f1a446e291fa1b70b31
narek <narek@DESKTOP-98JS365>
2024-06-20 10:49:15 +0300

    Добавьте файлы проекта.

---
+++ 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 5;
+        long factorial = CalculateFactorial(number);
+        Console.WriteLine($"Факториал числа {number} равен {factorial}");
+    }
+
+    static long CalculateFactorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
